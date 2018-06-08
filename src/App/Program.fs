5bd0620d921a993657ede5542156ca027e4514b2
冬雨 <1627333977@qq.com>
2018-06-09 00:12:25 +0800

    first commit

---
+++ src/App/Program.fs
@@ -0,0 +1,41 @@
+﻿open System
+open Library
+
+open System.IO
+
+[<EntryPoint>]
+let main argv =
+    //#printfn
+    printfn "hello"
+    printfn "Nice command-line arguments! Here's what JSON.NET has to say about them:"
+
+    //#let
+    let testValue=0;
+
+    //#function
+    let sampleFunction1 x = x*x + 3
+
+    let result1 = sampleFunction1 4573
+
+    printfn "the res is:%d" result1
+
+    let mutable otherNumber = 2
+
+    otherNumber <- otherNumber + 1
+
+    printfn "'otherNumber' changed to be %d" otherNumber
+
+    let path = @"c:\temp\MyTest.txt";
+
+    let isPathExist=File.Exists path
+    printfn "isPathExist %b" isPathExist
+
+    let sr = File.OpenText path
+    let s=sr.ReadLine() 
+    printfn "s: %s" s
+
+    argv
+    |> Array.map getJsonNetJson
+    |> Array.iter (printfn "%s")
+
+    0 // return an integer exit code
